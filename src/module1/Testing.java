fa96a4cfd2f6bae6ca1404b501c947ff9329e609
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2014-10-02 16:23:45 +0100

    Completed the module (unchecked)

--- src/module1/Testing.java
+++ src/module1/Testing.java
@@ -10,7 +10,30 @@ public class Testing {
 		
 		double[] anArray  = {1.1, 2.3, 3.3};
 		System.out.println(anArray[1]);
-		System.out.println(0/0);
+		
+		
+		
+			//for loop prints 1-10
+			for (int i=0; i<=10; i++){
+				System.out.println(i);
+			}
+			//while loop prints 5 to -15
+			int j=5;
+			while (j>=-15){
+				System.out.println(j);
+				j--;
+			}
+			//either loop prints 2.4 to 14.9 step 0.5
+			double k=2.4;
+			while (k<15){
+				System.out.println(k);
+				k+=0.5;
+			}	
+			
+			long timeNow=System.currentTimeMillis();
+			System.out.println(timeNow);
+
+	
 
 	}
 
